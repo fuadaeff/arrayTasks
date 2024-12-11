d019ebe57200b7293cfc0676e36c9ff6314b5c8b
fuadaeff <fabdulla057@gmail.com>
2024-12-11 09:38:53 +0400

    Add files via upload

---
+++ arrayTask10/arrayTask10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace arrayTask10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            int max = numbers[0];
+            int min = numbers[0];
+            int indexmin = 0;
+            int indexmax = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                    indexmin = i;
+                }
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                    indexmax = i;
+                }
+            }
+            numbers[indexmin] = max;
+            numbers[indexmax] = min;
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
