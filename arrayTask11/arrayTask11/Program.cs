f90e8ac976155a381b9829b6a7a8a382e966cb0e
fuadaeff <fabdulla057@gmail.com>
2024-12-11 09:40:15 +0400

    Add files via upload

---
+++ arrayTask11/arrayTask11/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace arrayTask11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            start = start - 1;
+            end = end - 1;
+            int sum = 0;
+            for (int i = start; i <= end; i++) { 
+                sum = sum + numbers[i];
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
