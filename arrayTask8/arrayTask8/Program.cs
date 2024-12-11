ec567bd703618276d9b0797a05bdc85f895f388f
fuadaeff <fabdulla057@gmail.com>
2024-12-11 09:37:48 +0400

    Add files via upload

---
+++ arrayTask8/arrayTask8/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace arrayTask8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+            int max = numbers[0];
+            foreach (int number in numbers) {
+                if (number > max) { 
+                    max = number;
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
