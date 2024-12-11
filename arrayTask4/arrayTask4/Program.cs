b767f27f736745409b6d7f953cf2ee0402d122e3
fuadaeff <fabdulla057@gmail.com>
2024-12-11 09:34:14 +0400

    Add files via upload

---
+++ arrayTask4/arrayTask4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace arrayTask4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int sum = 0;
+            foreach (int i in numbers) { 
+                if(i % 2 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
