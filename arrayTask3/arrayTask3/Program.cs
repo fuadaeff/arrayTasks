cad69ae3a8b9e27c088445d7ca8fe45ad4c8fe79
fuadaeff <fabdulla057@gmail.com>
2024-12-11 09:33:43 +0400

    Add files via upload

---
+++ arrayTask3/arrayTask3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace arrayTask3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            for (int i = numbers.Length - 1; i >= 0; i--) {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
