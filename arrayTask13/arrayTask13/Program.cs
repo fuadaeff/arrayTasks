9c39783edc22e0478f37e844fcb60e82429a2042
fuadaeff <fabdulla057@gmail.com>
2024-12-11 09:41:24 +0400

    Add files via upload

---
+++ arrayTask13/arrayTask13/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace arrayTask13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3,4, 5, 6, 7 };
+            int[] reversed = new int[numbers.Length];
+            int index = 0;
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                reversed[index] = numbers[i];
+                index = index + 1;
+            }
+            foreach (int number in reversed)
+            {
+
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
