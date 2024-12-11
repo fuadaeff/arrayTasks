b6f07dec389741c659b43fae6142ce68f50001fe
fuadaeff <fabdulla057@gmail.com>
2024-12-11 09:32:37 +0400

    Add files via upload

---
+++ arrayTask2/arrayTask2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace arrayTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 1, 2, 3, 4, 5, 6, 7 };
+            int sum = 0;
+            foreach (int i in number) { 
+                sum = sum + i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
