dbab4a04871e124ad7c5b74615fad6bec82a3920
fuadaeff <fabdulla057@gmail.com>
2024-12-11 09:40:49 +0400

    Add files via upload

---
+++ arrayTask12/arrayTask12/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace arrayTask12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int sum = 0;
+            decimal edediorta = 0;
+            int count = numbers.Length;
+            foreach (int i in numbers)
+            {
+                sum = sum + i;
+            }
+
+            edediorta = sum / count;
+            Console.WriteLine(edediorta);
+        }
+
+    }
+}
