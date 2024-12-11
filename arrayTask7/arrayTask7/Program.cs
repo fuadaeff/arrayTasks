0bf7864537576575bafb87237fbe4ef06314b957
fuadaeff <fabdulla057@gmail.com>
2024-12-11 09:37:15 +0400

    Add files via upload

---
+++ arrayTask7/arrayTask7/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace arrayTask7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());  
+            int b = int.Parse(Console.ReadLine());
+            int c = a;a = b;b = c;
+            Console.WriteLine(a,b);
+        }
+    }
+}
