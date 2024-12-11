9649ca4e4129bbcdd2a0cb7e0c1a127d7a595801
fuadaeff <fabdulla057@gmail.com>
2024-12-11 09:38:08 +0400

    Add files via upload

---
+++ arrayTask9/arrayTask9/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace arrayTask9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6 }; 
+            int min = numbers[0];
+            foreach (int number in numbers) {
+                if (number < min) {
+                    min = number;
+                }
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
