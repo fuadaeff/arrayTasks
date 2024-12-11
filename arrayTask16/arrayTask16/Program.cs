b478a79c9d09f5071bf102a4b29d38f73d341094
fuadaeff <fabdulla057@gmail.com>
2024-12-11 09:41:55 +0400

    Add files via upload

---
+++ arrayTask16/arrayTask16/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace arrayTask16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            int n = 0;
+            for (int i = 1; i < numbers.Length; i++) {
+                if (numbers[i-1] == numbers[i]) {
+                    Console.WriteLine("duplicate.");
+                    n = n + 1;
+                }
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
